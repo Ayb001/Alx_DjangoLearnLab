44748047f5f999a4e664181ecff1b1aee8cabfed
Ayb001 <aghjedimayoub@gmail.com>
2025-07-26 12:18:44 +0100

    test

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Book, Library, UserProfile
 
+
+
 # Function-based view to list all books
 def list_books(request):
     """
@@ -75,7 +77,7 @@ def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 # Permission-based views for book operations
-@permission_required('relationship_app.can_add_book')
+@permission_required('relationship_app.canaddbook', raise_exception=True)
 def add_book(request):
     """
     View to add a new book - requires can_add_book permission
@@ -85,7 +87,7 @@ def add_book(request):
         pass
     return render(request, 'relationship_app/add_book.html')
 
-@permission_required('relationship_app.can_change_book')
+@permission_required('relationship_app.canchangebook', raise_exception=True)
 def edit_book(request, pk):
     """
     View to edit a book - requires can_change_book permission
@@ -99,7 +101,7 @@ def edit_book(request, pk):
     except Book.DoesNotExist:
         return HttpResponse("Book not found", status=404)
 
-@permission_required('relationship_app.can_delete_book')
+@permission_required('relationship_app.candeletebook', raise_exception=True)
 def delete_book(request, pk):
     """
     View to delete a book - requires can_delete_book permission
